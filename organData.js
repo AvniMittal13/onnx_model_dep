7a1a28fb30d57d2ffcf45f4b681c1b879a6cdc41
Avni Mittal <avni.mittal2002@gmail.com>
2024-03-22 12:20:34 +0530

    rsna 128

--- organData.js
+++ organData.js
@@ -6,18 +6,18 @@ var organData = {
       output_var_name : "3654"
 
     },
-    rsna: {
-      file_name: "models\\nca_model_rsna_256.onnx",
-      required_dims: [256, 256, 96],
-      input_var_name : "x.1",
-      output_var_name : "3723"
-    }
     // rsna: {
-    //   file_name: "models\\nca_model_rsna_128.onnx",
-    //   required_dims: [128, 128, 48],
+    //   file_name: "models\\nca_model_rsna_256.onnx",
+    //   required_dims: [256, 256, 96],
     //   input_var_name : "x.1",
     //   output_var_name : "3723"
     // }
+    rsna: {
+      file_name: "models\\nca_model_rsna_128.onnx",
+      required_dims: [128, 128, 48],
+      input_var_name : "x.1",
+      output_var_name : "3723"
+    }
   };
   
 export default organData;
